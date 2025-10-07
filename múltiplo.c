a327cdd811bab8dc16f3cc7df24bb1bba35d1cf1
Miguel Matos <matosmiguel151@gmail.com>
2025-10-07 23:34:23 +0100

    Update múltiplo.c

--- múltiplo.c
+++ múltiplo.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
 
 int a,b;
-
-
-printf("Digite o primeiro número:  \n");
+   
 scanf("%d", &a);
-
-printf("Digite o segundo número:  \n");
 scanf("%d", &b);
 
-if( b==0 || a==0){
+if( b==0){
    printf("Não é possível verificar múltiplos com zero.\n");
    return 1;
 }
@@ -22,13 +17,9 @@ if(a % b == 0){
    printf("%d é múltiplo de %d.\n", a,b);
    
 }
-else if(b % a == 0 ){
-   printf("%d é múltiplo de %d.\n", b,a);
-   
-}
 
 else{
-   printf("Os números não são múltiplos entre si.\n");
+   printf("%d não é múltiplo de %d\n", a,b);
    
 }
 
